b09c2bb158bb05cf9c514aeb2c6c8a24adefa03e
Chengzhi Jia <j.chengzhi1989@gmail.com>
2016-05-15 23:26:06 -0700

    3

--- LeetCodeInObjective-C/LeetCodeInObjective-C/Problem1To10.m
+++ LeetCodeInObjective-C/LeetCodeInObjective-C/Problem1To10.m
@@ -67,6 +67,33 @@
     return result.next;
 }
 
+/*3. Longest Substring Without Repeating Characters
+ Given a string, find the length of the longest substring without repeating characters.
+ 
+ Examples:
+ 
+ Given "abcabcbb", the answer is "abc", which the length is 3.
+ 
+ Given "bbbbb", the answer is "b", with the length of 1.
+ 
+ Given "pwwkew", the answer is "wke", with the length of 3. Note that the answer must be a substring, "pwke" is a subsequence and not a substring.
+ */
+- (NSInteger)lengthOfLongestSubstring: (NSString *)s {
+    NSInteger len = s.length;
+    if (len < 2) return len;
+    NSInteger result = 0, lenNow = 0;
+    NSMutableDictionary *indexDict = [[NSMutableDictionary alloc]init];
+    for (NSInteger index = 0; index < len; index++) {
+        lenNow++;
+        NSString *charStr = [NSString stringWithFormat:@"%c",[s characterAtIndex:index]];
+        if ([indexDict.allKeys containsObject:charStr]) {
+            lenNow = MIN(lenNow, index - [[indexDict.allValues objectAtIndex:index] integerValue]);
+        }
+        [indexDict setObject:[NSNumber numberWithInteger:index] forKey:charStr];
+        result = MAX(result, lenNow);
+    }
+    return result;
+}
 
 @end
 
