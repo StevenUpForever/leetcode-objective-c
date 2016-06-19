6b8ce07ea76789ccf5ecbafa9e54f78819f11a72
Chengzhi Jia <j.chengzhi1989@gmail.com>
2016-06-19 12:00:32 -0700

    subsets

--- LeetCodeInObjective-C/LeetCodeInObjective-C/Problem71To80.m
+++ LeetCodeInObjective-C/LeetCodeInObjective-C/Problem71To80.m
@@ -32,20 +32,16 @@
  ]
  */
 - (NSArray *)subsets: (NSArray *)nums {
-    nums = [nums sortedArrayUsingComparator:^NSComparisonResult(id  _Nonnull obj1, id  _Nonnull obj2) {
-        return [obj1 integerValue] > [obj2 integerValue];
-    }];
-    if (nums.count == 0) return nil;
+    int len = 1 << nums.count;
     NSMutableArray *result = [[NSMutableArray alloc]init];
-    NSInteger n = 1 << nums.count;
-    for (NSInteger i = 0; i < n; i++) {
-        NSMutableArray *combineArray = [[NSMutableArray alloc]init];
-        for (NSInteger j = 0; j < nums.count; j++) {
-            //Fetch the higest binary number and AND with all possible binary numbers from index of array to check if any binary number of index exist and save it in the detail array
-            //找到当前可能的最高位二进制数，查找数组下标二进制数所有在这个最高位之后的，加入当前数组
-            if ((i & (1<<j)) != 0) [combineArray addObject:nums[j]];
+    for (int i = 0; i < len; i++) {
+        NSMutableArray *subset = [[NSMutableArray alloc]init];
+        for (int j = 0; j < nums.count; j++) {
+            if (i & ((1 << j) != 0)) {
+                [subset addObject:nums[j]];
+            }
         }
-        [result addObject:combineArray];
+        [result addObject:subset];
     }
     return result;
 }
