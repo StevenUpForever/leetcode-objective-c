7ba2b9cf52325e8928ab638b30f1dc494e75d226
Chengzhi Jia <j.chengzhi1989@gmail.com>
2017-08-14 22:46:16 -0700

    12. Integer to Roman

---
+++ LeetCode_Objective-C/LeetCode_Objective-C/Integer_to_Roman.h
@@ -0,0 +1,15 @@
+//
+//  Integer_to_Roman.h
+//  LeetCode_Objective-C
+//
+//  Created by Chengzhi Jia on 8/14/17.
+//  Copyright © 2017 Chengzhi Jia. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Integer_to_Roman : NSObject
+
+- (NSString *)intToRoman: (NSInteger)num;
+
+@end
