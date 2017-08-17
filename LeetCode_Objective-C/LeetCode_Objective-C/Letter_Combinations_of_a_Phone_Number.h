557458c298eb5b62737da63e783ed369a9db5474
c0j00cs <j.chengzhi1989@gmail.com>
2017-08-16 22:06:17 -0700

    17. Letter Combinations of a Phone Number

---
+++ LeetCode_Objective-C/LeetCode_Objective-C/Letter_Combinations_of_a_Phone_Number.h
@@ -0,0 +1,16 @@
+//
+//  Letter_Combinations_of_a_Phone_Number.h
+//  LeetCode_Objective-C
+//
+//  Created by Chengzhi Jia on 8/16/17.
+//  Copyright © 2017 Chengzhi Jia. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Letter_Combinations_of_a_Phone_Number : NSObject
+
+- (NSArray *)letterCombinationsS1: (NSString *)digits;
+- (NSArray *)letterCombinationsS2: (NSString *)digits;
+
+@end
