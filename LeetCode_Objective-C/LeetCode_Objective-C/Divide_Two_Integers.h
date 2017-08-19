75f1313ad28c733a729396b41eded3b6165d52fd
Chengzhi Jia <j.chengzhi1989@gmail.com>
2017-08-18 22:01:04 -0700

    29. Divide Two Integers

---
+++ LeetCode_Objective-C/LeetCode_Objective-C/Divide_Two_Integers.h
@@ -0,0 +1,16 @@
+//
+//  Divide_Two_Integers.h
+//  LeetCode_Objective-C
+//
+//  Created by Chengzhi Jia on 8/18/17.
+//  Copyright © 2017 Chengzhi Jia. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Divide_Two_Integers : NSObject
+
+- (NSInteger)divideS1: (NSInteger)dividend divisor: (NSInteger)divisor;
+- (NSInteger)divideS2: (NSInteger)dividend divisor: (NSInteger)divisor;
+
+@end
