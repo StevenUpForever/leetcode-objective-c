80a3079ae8e90731d1232254437c0987618edd12
Chengzhi Jia <j.chengzhi1989@gmail.com>
2017-08-18 22:08:35 -0700

    22. Generate Parentheses

---
+++ LeetCode_Objective-C/LeetCode_Objective-C/Generate_Parentheses.h
@@ -0,0 +1,15 @@
+//
+//  Generate_Parentheses.h
+//  LeetCode_Objective-C
+//
+//  Created by Chengzhi Jia on 8/18/17.
+//  Copyright © 2017 Chengzhi Jia. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Generate_Parentheses : NSObject
+
+- (NSArray *)generateParenthesis: (NSInteger)n;
+
+@end
