9cecfe5668c2b533a8798216148d06ae0e517751
Chengzhi Jia <j.chengzhi1989@gmail.com>
2017-08-19 22:25:23 -0700

    23. Merge k Sorted Lists

---
+++ LeetCode_Objective-C/LeetCode_Objective-C/Merge_k_Sorted_Lists.h
@@ -0,0 +1,15 @@
+//
+//  Merge_k_Sorted_Lists.h
+//  LeetCode_Objective-C
+//
+//  Created by Chengzhi Jia on 8/19/17.
+//  Copyright © 2017 Chengzhi Jia. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Merge_k_Sorted_Lists : NSObject
+
+- (ListNode *)mergeKListsS2: (NSArray<ListNode *> *)lists;
+
+@end
