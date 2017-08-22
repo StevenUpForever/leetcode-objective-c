b0c60b1735269a7d3e62ce0663b71e6582ed55e5
Chengzhi Jia <j.chengzhi1989@gmail.com>
2017-08-21 22:59:56 -0700

    30. Substring with Concatenation of All Words

---
+++ LeetCode_Objective-C/LeetCode_Objective-C/Substring_with_Concatenation_of_All_Words.h
@@ -0,0 +1,16 @@
+//
+//  Substring_with_Concatenation_of_All_Words.h
+//  LeetCode_Objective-C
+//
+//  Created by Chengzhi Jia on 8/21/17.
+//  Copyright © 2017 Chengzhi Jia. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Substring_with_Concatenation_of_All_Words : NSObject
+
+- (NSArray *)findSubstringS1: (NSString *)s words: (NSArray<NSString *> *)words;
+- (NSArray *)findSubstringS2: (NSString *)s words: (NSArray<NSString *> *)words;
+
+@end
