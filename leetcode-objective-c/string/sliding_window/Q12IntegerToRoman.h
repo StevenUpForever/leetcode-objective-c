0fb6374a04bcbafd3b6c551806da19522ef37c8e
c0j00cs <cjia@walmartlabs.com>
2019-02-18 15:05:30 -0800

    refactor 11-20

---
+++ leetcode-objective-c/string/sliding_window/Q12IntegerToRoman.h
@@ -0,0 +1,15 @@
+//
+//  Integer_to_Roman.h
+//  LeetCode_Objective-C
+//
+//  Created by Chengzhi Jia on 8/14/17.
+//  Copyright © 2017 Chengzhi Jia. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Q12IntegerToRoman : NSObject
+
+- (NSString *)intToRoman: (NSInteger)num;
+
+@end
