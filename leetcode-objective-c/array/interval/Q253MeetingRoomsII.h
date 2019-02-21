9de3104e22676e0888e055c010665b75537dc8d8
c0j00cs <cjia@walmartlabs.com>
2019-02-20 23:53:19 -0800

    253. Meeting Rooms II

---
+++ leetcode-objective-c/array/interval/Q253MeetingRoomsII.h
@@ -0,0 +1,20 @@
+//
+//  Q253MeetingRoomsII.h
+//  leetcode-objective-c
+//
+//  Created by Chengzhi Jia on 2/20/19.
+//  Copyright © 2019 com.cj. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@class Interval;
+@interface Q253MeetingRoomsII : NSObject
+
+- (NSInteger)minMeetingRooms: (NSArray<Interval *> *)intervals;
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ leetcode-objective-c/array/interval/Q253MeetingRoomsII.m
@@ -0,0 +1,68 @@
+//
+//  Q253MeetingRoomsII.m
+//  leetcode-objective-c
+//
+//  Created by Chengzhi Jia on 2/20/19.
+//  Copyright © 2019 com.cj. All rights reserved.
+//
+
+#import "Q253MeetingRoomsII.h"
+#import "Interval.h"
+
+@implementation Q253MeetingRoomsII
+
+//Difficulty: Medium
+//TAG: Facebook
+//TAG: Google
+//TAG: Snap
+//TAG: Uber
+//TAG: Apple
+//TAG: Array
+//TAG: range
+
+/**
+ * 253. Meeting Rooms II
+ * Given an array of meeting time intervals consisting of start and end times [[s1,e1],[s2,e2],...] (si < ei), find the minimum number of conference rooms required.
+ 
+ For example,
+ Given [[0, 30],[5, 10],[15, 20]],
+ return 2.
+ */
+
+/*
+* Solution:
+* Don't need to consider every interval as a single interval but two numbers, start and end, gather all starts
+* and ends
+*
+* sort starts and ends in ascending order
+* compare start and end, if start < end, need room, room++
+* otherwise move end try to find the end > start
+*
+* Time: O(n + 2nlogn + 2n) = O(nlogn)
+* Space: O(n)
+*/
+
+- (NSInteger)minMeetingRooms: (NSArray<Interval *> *)intervals {
+    NSMutableArray<NSNumber *> *start = [NSMutableArray new],
+    *end = [NSMutableArray new];
+    for (NSInteger i = 0; i < intervals.count; i++) {
+        [start addObject:@(intervals[i].start)];
+        [end addObject:@(intervals[i].end)];
+    }
+    [start sortUsingComparator:^NSComparisonResult(NSNumber  * _Nonnull obj1, NSNumber * _Nonnull obj2) {
+        return [obj1 compare:obj2];
+    }];
+    [end sortUsingComparator:^NSComparisonResult(NSNumber  * _Nonnull obj1, NSNumber * _Nonnull obj2) {
+        return [obj1 compare:obj2];
+    }];
+    NSInteger count = 0;
+    NSInteger endIndex = 0;
+    for (NSInteger i = 0; i < start.count; i++) {
+        if (start[i].integerValue < end[endIndex].integerValue) {
+            count++;
+        } else endIndex++;
+    }
+    return count;
+}
+
+@end
